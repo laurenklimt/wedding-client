c31aec5119811fe70f2284f9ce243067810ee5f3
laurenklimt <lauren.klimt@gmail.com>
2023-08-09 14:24:16 +1000

    remove by kim

--- src/pages/Registry/Registry.js
+++ src/pages/Registry/Registry.js
@@ -15,7 +15,6 @@ function Registry() {
                     <p><a href="https://www.petersofkensington.com.au/gift-registry/giftregistrysearch/giftregistrydetails/?Id=b5059e38-5cd7-45fe-8289-b1f4d3968cb9" target="_blank" rel="noreferrer">Peter's of Kensington (registry no. 1755200)<i className="material-icons">open_in_new</i></a></p>
                     <p><a href="https://thesilverco.com/wedding-registries/" target="_blank" rel="noreferrer">Antique Silver Co. (Judaica)<i className="material-icons">open_in_new</i></a></p>
                     <p><a href="https://www.thehintregistry.com.au/browse/laurenandbenji" target="_blank" rel="noreferrer">Wishing Well<i className="material-icons">open_in_new</i></a></p>
-                    <p>We have also put together a list at <b>By Kim</b> in Rose Bay. To purchase a gift from this list please visit in store or call on <a href="tel:0293713377">(02) 9371 3377</a></p>
                     <div className='event-button'>
                         <button onClick={() => navigate("/event-details")}>EVENT DETAILS</button>
                     </div>
